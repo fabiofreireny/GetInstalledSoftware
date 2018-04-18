572a994cc03b2fa7af6463426055c8b2108e22d1
Fabio Freire (laptop) <fabio@fabio.nyc>
2018-04-18 16:32:54 -0400

    Cleaned up code

--- GetInstalledSoftware.ps1
+++ GetInstalledSoftware.ps1
@@ -22,31 +22,33 @@ $exclude = @(
     "Microsoft Exchange Server Language Pack"
 )
 
+$sb32 = {
+    Get-ChildItem -Path HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall | 
+    Get-ItemProperty | Select-Object -Property DisplayName, DisplayVersion | Where DisplayName 
+}
+
+$sb64 = {
+    Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall | 
+    Get-ItemProperty | Select-Object -Property DisplayName, DisplayVersion | Where DisplayName  
+}
 #Get full list of installed SW (32 and 64 bit)
 #WMI is too slow, going directly to registry instead
 if ($ComputerName) {
-    $Dump32bit = Invoke-Command -ComputerName $ComputerName -ScriptBlock { `
-        Get-ChildItem -Path HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall | `
-        Get-ItemProperty | Select-Object -Property DisplayName, DisplayVersion | Where DisplayName -GT "" }
-    $Dump64bit = Invoke-Command -ComputerName $ComputerName -ScriptBlock { `
-        Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall | `
-        Get-ItemProperty | Select-Object -Property DisplayName, DisplayVersion | Where DisplayName -GT "" }
+    $Dump32bit = Invoke-Command -ComputerName $ComputerName -ScriptBlock $sb32
+    $Dump64bit = Invoke-Command -ComputerName $ComputerName -ScriptBlock $sb64
+        
 } else {
-    $Dump32bit = Get-ChildItem -Path HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall | `
-        Get-ItemProperty | Select-Object -Property DisplayName, DisplayVersion | Where DisplayName -GT ""
-    $Dump64bit = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall | `
-        Get-ItemProperty | Select-Object -Property DisplayName, DisplayVersion | Where DisplayName -GT ""
+    $Dump32bit = Invoke-Command -ScriptBlock $sb32
+    $Dump64bit = Invoke-Command -ScriptBlock $sb64
 }
 $SoftwareDump = ($Dump32bit + $Dump64bit)
 
-#Exclude filtered items
 $installedSoftware = @()
 
-#Doing this for improved performance (it will be used many times)
 $exclude = ($exclude -join '|')
 
 #remove excluded items, remove duplicate items and sort output
-$installedSoftware = $SoftwareDump | ? displayName -notmatch $exclude | `
+$installedSoftware = $SoftwareDump | ? displayName -notmatch $exclude |
     select -Property displayname, displayversion -Unique | Sort-Object -Property DisplayName
 
 $installedSoftware
